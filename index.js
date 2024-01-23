e27c0b6b035a8fd2410d8829413444f72cb6ae8e
kazimustafa95 <kazi.mustafa95@gmail.com>
2024-01-23 18:28:05 +0500

    add initail commit

---
+++ index.js
@@ -0,0 +1,59 @@
+// 1: reverse an integer in JavaScript without converting it to a string
+
+ function reverseInt(num){
+    let reversed = 0;
+    while(num!=0){
+        reversed = reversed*10 + num%10;
+        num = Math.floor(num/10);
+
+    }
+    return reversed;
+
+ }
+
+ console.log(reverseInt(6576576));
+
+
+// 2: reverse an integer by converting it to a string
+
+    function reverseInt2(num){
+        let reversed = num.toString().split('').reverse().join('');
+        return parseInt(reversed);
+    }
+
+    console.log(reverseInt2(6576576));
+
+
+// 3: given number is prime or not 
+
+function isPrime(num){
+    if(num<=1){
+        return false;
+    }
+    for(let i=2;i<num;i++){
+        if(num%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+//4:  given number is even or not 
+
+function isEven(num){
+    return num%2==0;
+}
+
+//5: given number is odd or not
+
+function isOdd(num){
+    return num%2!=0;
+}
+
+
+
+
+
+
+
+
